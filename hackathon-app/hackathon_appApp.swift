0ffb0b7b73be917b957fad589e2741b137dd31b1
kyoya <kyoyaprog@gmail.com>
2023-03-02 17:13:01 +0900

    initail commit

---
+++ hackathon-app/hackathon_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackathon_appApp.swift
+//  hackathon-app
+//
+//  Created by 松山響也 on 2023/03/02.
+//
+
+import SwiftUI
+
+@main
+struct hackathon_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
