a1b77139e9eeffe34878e2bd7da6dd5648206446
kyoya <kyoyaprog@gmail.com>
2023-03-10 03:22:55 +0900

    fix

--- hackathon-app/CarePlan/Presenter/CarePlanListPresenter.swift
+++ hackathon-app/CarePlan/Presenter/CarePlanListPresenter.swift
@@ -7,7 +7,7 @@ class CarePlanListPresenter: ObservableObject {
             switch result {
             case .success(let model):
                 self?.carePlans = model.map({ carePlan in
-                    CarePlanViewModel(id: carePlan.id.description, clientName: carePlan.author!, clientAge: String(carePlan.client.age), author: carePlan.author ?? "", facilityName: carePlan.facilityName ?? "", carePolicy: carePlan.carePolicy ?? "", specifiedService: carePlan.specifiedService ?? "", careCommitteeOpinion: carePlan.careCommitteeOpinion ?? "", resultAnalyze: carePlan.resultAnalyze!, updatedAt: carePlan.updatedAt)
+                    CarePlanViewModel(id: carePlan.id.description, clientName: carePlan.client.name, clientAge: String(carePlan.client.age), author: carePlan.author ?? "", facilityName: carePlan.facilityName ?? "", carePolicy: carePlan.carePolicy ?? "", specifiedService: carePlan.specifiedService ?? "", careCommitteeOpinion: carePlan.careCommitteeOpinion ?? "", resultAnalyze: carePlan.resultAnalyze!, updatedAt: carePlan.updatedAt)
                 })
             case .failure(let failure):
                 break
--- hackathon-app/CarePlan/View/ViewComponents/CarePlanListItem.swift
+++ hackathon-app/CarePlan/View/ViewComponents/CarePlanListItem.swift
@@ -3,10 +3,15 @@ import SwiftUI
 struct CarePlanListItem: View {
     let model: CarePlanViewModel
     var body : some View {
-        return HStack {
+        return VStack {
             Text(model.updatedAt + "編集")
-            Text(model.clientName + "さん")
-            Text(model.author + "作")
+                Text(model.clientName + "さん")
+                Text("記述者名：" + model.author )
         }
+    }
+}
+struct CarePlanListItemPreviewProvider: PreviewProvider {
+    static var previews: some View {
+        CarePlanListItem(model: CarePlanViewModel(id: "", clientName: "田中角栄", clientAge: "90", author: "田中陽子", facilityName: "", carePolicy: "", specifiedService: "", careCommitteeOpinion: "", resultAnalyze: "", updatedAt: "2022-02-28")).previewLayout(.fixed(width: 160, height: 50))
     }
 }
